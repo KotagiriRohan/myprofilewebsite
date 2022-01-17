add7b10e05614869ef51198e0288ba03c0e66673
Rohan Kotagiri <rohankotagiri10@gmail.com>
2022-01-17 18:55:59 +0530

    update

---
+++ src/Banner.js
@@ -0,0 +1,11 @@
+import bannerimg from "./download2.jpg";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img src={bannerimg} alt="" />
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="nav-bar">
+      <div className="nav-bar-name">Rohan Kotagiri</div>
+      <div className="nav-bar-links">
+        <a href="/">About</a>
+        <a href="/">Projects</a>
+        <a href="/">Drawings</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
