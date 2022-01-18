ba082d015f4bf8326a4fcd3331291f34744b264e
Rohan Kotagiri <rohankotagiri10@gmail.com>
2022-01-19 01:43:24 +0530

    Update

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,8 @@
-import bannerimg from "./stars.jpg";
-import profileimg from "./profileimg.jpg";
+import bannerimg from "./polygon.jpg";
+// import profileimg from "./profileimg.jpg";
 import React from "react";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { SiCodechef } from "react-icons/si";
 const Banner = () => {
   return (
     <header
@@ -11,10 +13,22 @@ const Banner = () => {
         backgroundPosition: "center center",
       }}
     >
-      <div className="banner-profileimg">
+      {/* <div className="banner-profileimg">
         <img src={profileimg} alt="" />
-      </div>
+      </div> */}
       <h1>Computer Science Student</h1>
+      <p className="banner-collegename">Manipal University Jaipur</p>
+      <div className="banner-icons">
+        <a href="/">
+          <BsLinkedin />
+        </a>
+        <a href="/">
+          <BsGithub />
+        </a>
+        <a href="/">
+          <SiCodechef />
+        </a>
+      </div>
     </header>
   );
 };
