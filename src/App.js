3f4ca012637f15f8ebe0722097579fce23f09851
Rohan Kotagiri <rohankotagiri10@gmail.com>
2022-01-20 23:13:19 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Banner from "./Banner";
 import Navbar from "./Navbar";
+import ProjectCards from "./ProjectCards";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <ProjectCards />
     </div>
   );
 }
