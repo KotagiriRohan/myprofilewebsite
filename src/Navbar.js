5e5dd43d5f77aff9e2b625e21a752011b81a0872
Rohan Kotagiri <rohankotagiri10@gmail.com>
2022-01-21 18:02:11 +0530

    add scroll change

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,27 @@
+import { useState, useEffect } from "react";
+
 const Navbar = () => {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav-bar">
-      <div className="nav-bar-name">Rohan Kotagiri</div>
-      <div className="nav-bar-links">
-        <a href="/">About</a>
-        <a href="/">Projects</a>
-        <a href="/">Drawings</a>
+    <div className={`nav-bar ${show && "nav-bar__black"}`}>
+      <div className="nav-bar-padding">
+        <div className="nav-bar-name">Rohan Kotagiri</div>
+        <div className="nav-bar-links">
+          <a href="/">About</a>
+          <a href="/">Projects</a>
+          <a href="/">Drawings</a>
+        </div>
       </div>
     </div>
   );
