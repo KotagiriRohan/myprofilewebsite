c72d729a407ebd38752b9c17313cae8bca0394cf
Rohan Kotagiri <rohankotagiri10@gmail.com>
2022-01-21 18:02:24 +0530

    Add all the projects

--- src/ProjectCards.js
+++ src/ProjectCards.js
@@ -1,4 +1,7 @@
-import polygonimg from "./polygon.jpg";
+import netflix from "./netflix.jpg";
+import asl from "./asl.png";
+import mrs from "./MRS.jpg";
+import { SiPython, SiReact } from "react-icons/si";
 const ProjectCards = () => {
   return (
     <div className="projects">
@@ -7,21 +10,47 @@ const ProjectCards = () => {
       </div>
       <div className="cards">
         <div className="card">
-          <img src={polygonimg} alt="" />
-          <div className="card-title">XYZ</div>
-          <div className="card-desc">ehwfhijweijfwejfowjfwiojefijjfjfkdf</div>
+          <img src={asl} alt="" />
+          <div className="card-title">ASL Detection</div>
+          <div className="card-desc">
+            Image Detection project to classify different ASL alphabets using
+            TensorFlow and Mediapipe API.
+          </div>
+          <div className="card-projectlang">
+            <SiPython />
+            <p>Python</p>
+          </div>
         </div>
         <div className="card">
-          <img src={polygonimg} alt="" />
-          <div className="card-title">XYZ</div>
-          <div className="card-desc">ehwfhijweijfwejfowjfwiojefijjfjfkdf</div>
+          <img src={mrs} alt="" />
+          <div className="card-title">Recommendation System</div>
+          <div className="card-desc">
+            Movie Recommendations using Demographic Filtering and Content Based
+            Filtering in Python.
+          </div>
+          <div className="card-projectlang">
+            <SiPython />
+            <p>Python</p>
+          </div>
         </div>
         <div className="card">
-          <img src={polygonimg} alt="" />
-          <div className="card-title">XYZ</div>
-          <div className="card-desc">ehwfhijweijfwejfowjfwiojefijjfjfkdf</div>
+          <img src={netflix} alt="" />
+          <div className="card-title">Netflix Clone</div>
+          <div className="card-desc">
+            Single dynamic web page using React JS and TMDB API.
+          </div>
+          <div className="card-projectlang">
+            <SiReact />
+            <p>React JS</p>
+          </div>
         </div>
       </div>
+      <div className="project-button">
+        <a href="/">
+          <button>Show More</button>
+        </a>
+      </div>
+      <div></div>
     </div>
   );
 };
