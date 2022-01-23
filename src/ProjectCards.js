6452bf73d0ecd351f5f9ff31765f4724b2390d22
Rohan Kotagiri <rohankotagiri10@gmail.com>
2022-01-23 16:09:52 +0530

    made it dynamic

--- src/ProjectCards.js
+++ src/ProjectCards.js
@@ -1,56 +1,32 @@
-import netflix from "./netflix.jpg";
-import asl from "./asl.png";
-import mrs from "./MRS.jpg";
 import { SiPython, SiReact } from "react-icons/si";
-const ProjectCards = () => {
+import useFetch from "./useFetch";
+
+const ProjectCards = ({ num }) => {
+  const { data, isPending, error } = useFetch(num);
+
   return (
     <div className="projects">
       <div className="project-header">
         <h1>Projects</h1>
       </div>
       <div className="cards">
-        <div className="card">
-          <img src={asl} alt="" />
-          <div className="card-title">ASL Detection</div>
-          <div className="card-desc">
-            Image Detection project to classify different ASL alphabets using
-            TensorFlow and Mediapipe API.
-          </div>
-          <div className="card-projectlang">
-            <SiPython />
-            <p>Python</p>
-          </div>
-        </div>
-        <div className="card">
-          <img src={mrs} alt="" />
-          <div className="card-title">Recommendation System</div>
-          <div className="card-desc">
-            Movie Recommendations using Demographic Filtering and Content Based
-            Filtering in Python.
-          </div>
-          <div className="card-projectlang">
-            <SiPython />
-            <p>Python</p>
-          </div>
-        </div>
-        <div className="card">
-          <img src={netflix} alt="" />
-          <div className="card-title">Netflix Clone</div>
-          <div className="card-desc">
-            Single dynamic web page using React JS and TMDB API.
-          </div>
-          <div className="card-projectlang">
-            <SiReact />
-            <p>React JS</p>
-          </div>
-        </div>
+        {isPending && <div>Loading...</div>}
+        {error && <div>{error}</div>}
+        {data &&
+          data.length > 0 &&
+          data.map((project) => (
+            <div className="card" key={project.id}>
+              <img src={process.env.PUBLIC_URL + project.imagepath} alt="" />
+              <div className="card-title">{project.title}</div>
+              <div className="card-desc">{project.description}</div>
+              <div className="card-projectlang">
+                {project.language === "Python" && <SiPython />}
+                {project.language === "React JS" && <SiReact />}
+                <p>{project.language}</p>
+              </div>
+            </div>
+          ))}
       </div>
-      <div className="project-button">
-        <a href="/">
-          <button>Show More</button>
-        </a>
-      </div>
-      <div></div>
     </div>
   );
 };
