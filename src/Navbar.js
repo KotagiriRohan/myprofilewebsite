ea247cc430a0621a621444e5f2a0718b25dc6d74
Rohan Kotagiri <rohankotagiri10@gmail.com>
2022-01-31 22:57:28 +0530

    Update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,28 +1,64 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
 
-const Navbar = () => {
+const Navbar = ({ scroll }) => {
   const [show, handleShow] = useState(false);
+  const [dropdown, handleDropdown] = useState(false);
+
+  const scrooly = () => {
+    if (window.scrollY >= 100) {
+      handleShow(true);
+    } else handleShow(false);
+  };
+  const resizes = () => {
+    if (window.innerWidth <= 1000) {
+      handleDropdown(true);
+    } else handleDropdown(false);
+  };
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if (window.scrollY >= 100) {
-        handleShow(true);
-      } else handleShow(false);
+      scrooly();
+    });
+    window.addEventListener("resize", () => {
+      resizes();
     });
     return () => {
-      window.removeEventListener("scroll");
+      window.removeEventListener("scroll", scrooly);
+      window.removeEventListener("resize", resizes);
     };
   }, []);
 
   return (
-    <div className={`nav-bar ${show && "nav-bar__black"}`}>
+    <div
+      className={`nav-bar ${scroll && show && "nav-bar__black"} ${
+        !scroll && "nav-bar__black"
+      }`}
+    >
       <div className="nav-bar-padding">
         <div className="nav-bar-name">Rohan Kotagiri</div>
-        <div className="nav-bar-links">
-          <a href="/">About</a>
-          <Link to="/projects">Projects</Link>
-          <a href="/">Drawings</a>
-        </div>
+        {dropdown && (
+          <div className="dropdown">
+            <div className="dropbtn">
+              <FiMenu />
+            </div>
+            <div className="dropdown-content">
+              <Link to="/">Home</Link>
+              <a href="/">About</a>
+              <Link to="/projects">Projects</Link>
+              <a href="/">Drawings</a>
+            </div>
+          </div>
+        )}
+        {!dropdown && (
+          <div className="nav-bar-links">
+            <Link to="/">Home</Link>
+            <a href="/">About</a>
+            <Link to="/projects">Projects</Link>
+            <a href="/">Drawings</a>
+          </div>
+        )}
       </div>
     </div>
   );
