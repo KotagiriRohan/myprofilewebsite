81ee2aa27b4556cd3a914afdd63e698b50c97b99
Rohan Kotagiri <rohankotagiri10@gmail.com>
2022-02-01 16:27:34 +0530

    footer

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-content">
+        <h1>Contact Me</h1>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
