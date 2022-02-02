b0dc03ca115373e8eb037db05ce069324a767cef
Rohan Kotagiri <rohankotagiri10@gmail.com>
2022-02-02 21:19:19 +0530

    carousel

---
+++ src/Carousel.js
@@ -0,0 +1,98 @@
+import React from "react";
+
+const Carousel = () => {
+  return (
+    <div className="carouselcomp">
+      <div className="carousel-header">
+        <h1>Drawings</h1>
+      </div>
+      <div class="container">
+        <div class="carousel">
+          <div
+            id="panel-1"
+            class="panel"
+            style={{
+              backgroundImage: `url(
+              "http://cdn.flipboard.com/dev_O/topics/exercise-157321849/large.jpg"
+            )`,
+            }}
+          ></div>
+          <div
+            id="panel-2"
+            class="panel"
+            style={{
+              backgroundImage: `url(
+              "http://cdn.flipboard.com/dev_O/topics/exercise-157321849/large.jpg"
+            )`,
+            }}
+          ></div>
+          <div
+            id="panel-3"
+            class="panel"
+            style={{
+              backgroundImage: `url(
+              "http://cdn.flipboard.com/dev_O/topics/exercise-157321849/large.jpg"
+            )`,
+            }}
+          ></div>
+          <div
+            id="panel-4"
+            class="panel"
+            style={{
+              backgroundImage: `url(
+              "http://cdn.flipboard.com/dev_O/topics/exercise-157321849/large.jpg"
+            )`,
+            }}
+          ></div>
+          <div
+            id="panel-5"
+            class="panel"
+            style={{
+              backgroundImage: `url(
+              "http://cdn.flipboard.com/dev_O/topics/exercise-157321849/large.jpg"
+            )`,
+            }}
+          ></div>
+          <div
+            id="panel-6"
+            class="panel"
+            style={{
+              backgroundImage: `url(
+              "http://cdn.flipboard.com/dev_O/topics/exercise-157321849/large.jpg"
+            )`,
+            }}
+          ></div>
+          <div
+            id="panel-7"
+            class="panel"
+            style={{
+              backgroundImage: `url(
+              "http://cdn.flipboard.com/dev_O/topics/exercise-157321849/large.jpg"
+            )`,
+            }}
+          ></div>
+          <div
+            id="panel-8"
+            class="panel"
+            style={{
+              backgroundImage: `url(
+              "http://cdn.flipboard.com/dev_O/topics/exercise-157321849/large.jpg"
+            )`,
+            }}
+          ></div>
+          <div
+            id="panel-9"
+            class="panel"
+            style={{
+              backgroundImage: `url(
+              "http://cdn.flipboard.com/dev_O/topics/exercise-157321849/large.jpg"
+            )`,
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
